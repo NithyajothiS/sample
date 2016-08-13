203ccda33e3d79dd868c101e6a22cba3f4e85506
NithyajothiS <nithyajothis4@gmail.com>
2016-08-13 13:07:19 +0530

    Create file1.c

---
+++ file1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int n;
+scanf("%d",&n);
+if(n==0)
+printf("%d is zero",n);
+else if(n>1)
+printf("%d is positive",n);
+else
+printf("%d is negative",n);
+return 0;
+}
